610a911df60baa81823b1d79464b3557de459096
LaVispera <lau.liino@gmail.com>
2017-10-17 14:49:16 +0200

    Mysko fel

---
+++ script.js
@@ -0,0 +1,33 @@
+let container = document.getElementById("container")
+let width = container.clientWidth;
+let height = container.clientHeight;
+let aspect = width / height;
+
+let scene = new THREE.Scene();
+//let camera = new THREE.PerspectiveCamera(50, aspect, 0.1, 1000);
+//console.log(camera);
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize(width, height);
+container.appendChild(renderer.domElement);
+
+let geometry = new THREE.CubeGeometry(1,1,1);
+let material = new THREE.MeshBasicMaterial({
+	wireframe: true,
+	color: 0x111111
+});
+
+let cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+//camera.position.z = 3
+
+function render() {
+	//requestAnimationFrame(render);
+
+	cube.rotation.x += 0.03;
+	cube.rotation.y += 0.03;
+
+	renderer.render(scene.camera);
+}
+
+render();
